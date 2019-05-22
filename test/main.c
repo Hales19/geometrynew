06175ae0692372102f91d1648b71e747de33a374
Hales19 <a.sarvetnikov@mail.ru>
2019-05-22 02:04:46 -0400

    Add chenge main.c in test

--- test/main.c
+++ test/main.c
@@ -10,7 +10,7 @@ int main(int argc, const char** argv)
 
 CTEST(peres1__test, result_test)
 {
-    // Given
+    
     const double x1 = 2;
     const double y1 = 2;
     const double r1 = 3;
@@ -18,17 +18,17 @@ CTEST(peres1__test, result_test)
     const double y2 = 3;
     const double r2 = 2;
 
-    // When
+    
     const double result = peres(x1, y1, r1, x2, y2, r2);
 
-    // Then
+    
     const double expected = 1;
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST(peres2__test, result_test)
 {
-    // Given
+    
     const double x1 = -5;
     const double y1 = -4;
     const double r1 = 2;
@@ -36,7 +36,7 @@ CTEST(peres2__test, result_test)
     const double y2 = 3;
     const double r2 = 2;
 
-    // When
+    
     const double result = peres(x1, y1, r1, x2, y2, r2);
 
     // Then
@@ -46,14 +46,14 @@ CTEST(peres2__test, result_test)
 
 CTEST(perimetrC__test, result_test)
 {
-    // Given
+    
     const double r = 2;
     const double PI = 3.1415;
 
-    // When
+    
     const double result = PerimetrC(r, PI);
 
-    // Then
+    
     const double expected = 12.566;
     const double tol = 0.001;
     ASSERT_DBL_NEAR_TOL(expected, result, tol);
@@ -61,29 +61,29 @@ CTEST(perimetrC__test, result_test)
 
 CTEST(perimetrT__test, result_test)
 {
-    // Given
+    
     const double a = 4;
     const double b = 6;
     const double c = 3;
 
-    // When
+    
     const double result = PerimetrT(a, b, c);
 
-    // Then
+    
     const double expected = 13;
     ASSERT_EQUAL(expected, result);
 }
 
 CTEST(squareC__test, result_test)
 {
-    // Given
+   
     const double r = 5;
     const double PI = 3.1415;
 
-    // When
+    
     const double result = SquareC(r, PI);
 
-    // Then
+    
     const double expected = 78.5375;
     const double tol = 0.0001;
     ASSERT_DBL_NEAR_TOL(expected, result, tol);
@@ -91,15 +91,15 @@ CTEST(squareC__test, result_test)
 
 CTEST(squareT__test, result_test)
 {
-    // Given
+    
     const double a = 4;
     const double b = 6;
     const double c = 3;
 
-    // When
+    
     const double result = SquareT(a, b, c);
 
-    // Then
+    
     const double expected = 5.332682;
     const double tol = 0.000001;
     ASSERT_DBL_NEAR_TOL(expected, result, tol);
